3c119c045208db8425a61da8c372f60a16534ecc
Tyga <tyga.dev.hotmail.de>
2013-11-27 00:24:54 +0100

    initil commit

---
+++ src/main/java/de/htw_berlin/f4/ai/kbe/App.java
@@ -0,0 +1,13 @@
+package de.htw_berlin.f4.ai.kbe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
