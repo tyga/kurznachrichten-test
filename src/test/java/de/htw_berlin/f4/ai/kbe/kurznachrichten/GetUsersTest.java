0ef020978fe09651a4f0294a48972082d8aaed59
Felix Wardelmann <felixwardelmann@msn.com>
2013-12-05 15:33:15 +0100

    deleteUser is tested, getUsers is not yet finished testing

--- src/test/java/de/htw_berlin/f4/ai/kbe/kurznachrichten/GetUsersTest.java
+++ src/test/java/de/htw_berlin/f4/ai/kbe/kurznachrichten/GetUsersTest.java
@@ -1,5 +1,26 @@
 package de.htw_berlin.f4.ai.kbe.kurznachrichten;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class GetUsersTest {
 
+	// /////////////////////////////////////////////////////
+	// getUserTests
+	// /////////////////////////////////////////////////////
+		
+	private ShortMessageService sms;
+	private String city = "irgendwo";
+	
+	@Test
+	public void getUsersTestReturnsCorrectListOfUsers(){
+		sms.createUser("user1", city);
+		sms.createUser("user2", city);
+		sms.createUser("user3", city);
+		assertNotNull(sms.getUsers());
+		//to do:	Set<user> mit den usern, die created wurden erzeugen
+		//			assertEquals(Set<>, sms.getUsers());
+		//problem:	kp, wie man für die User name und city setted
+	}
 }
