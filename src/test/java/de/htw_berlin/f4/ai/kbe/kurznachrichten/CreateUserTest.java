631018155613deecc059bedc72ab5980574c840f
Felix Wardelmann <felixwardelmann@msn.com>
2013-12-05 15:02:20 +0100

    createUser is tested

--- src/test/java/de/htw_berlin/f4/ai/kbe/kurznachrichten/CreateUserTest.java
+++ src/test/java/de/htw_berlin/f4/ai/kbe/kurznachrichten/CreateUserTest.java
@@ -2,34 +2,61 @@ package de.htw_berlin.f4.ai.kbe.kurznachrichten;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class CreateUserTest {
+public class CreateUserTest extends TestShortMessageServiceInit {
 
 	// /////////////////////////////////////////////////////
-		// createUserTests
-		// /////////////////////////////////////////////////////
+	// createUserTests
+	// /////////////////////////////////////////////////////
 	
-	private ShortMessageService sms;
+	//private ShortMessageService sms;
+	private String user = "user1";
 	
 	@Before
 	public void initialize(){
-		sms.createUser("user1", "berlin");
+		sms.createUser(user, "berlin");
 	}
 	
 	@Test (expected = IllegalArgumentException.class)
 	public void createUserTestNameAlreadyExists(){
-		sms.createUser("user1", "irgendwo");
+		sms.createUser(user, "irgendwo");
 	}
 	
 	@Test (expected = IllegalArgumentException.class)
 	public void createUserTestNameTooLong(){
-		
+		sms.createUser(STR_LENGTH_31, "irgendwo");
+	}
+	
+	@Test
+	public void createUserTestNameNotTooLong(){
+		sms.createUser(STR_LENGTH_30, "irgendwo");
 	}
 	
 	@Test (expected = IllegalArgumentException.class)
 	public void createUserTestNameTooShort(){
-		
+		sms.createUser(STR_LENGTH_3, "irgendwo");
+	}
+	
+	@Test
+	public void createUserTestNameNotTooShort(){
+		sms.createUser(STR_LENGTH_4, "irgendwo");
+	}
+	
+	@Test (expected = NullPointerException.class)
+	public void createUserTestNameNull(){
+		sms.createUser(null, "irgendwo");
+	}
+	
+	@Test (expected = NullPointerException.class)
+	public void createUserTestCityNull(){
+		sms.createUser(user, null);
+	}
+	
+	@After
+	public void cleanUp(){
+		sms.deleteUser(user);
 	}
 }
